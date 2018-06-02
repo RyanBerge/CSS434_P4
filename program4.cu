ba7fa1a4c6a0d5f1875fec632cc44cae46d1dc3a
Ryan Berge <ryanberge13@gmail.com>
2018-06-02 15:29:00 -0700

    Implement optimized thingy

--- program4.cu
+++ program4.cu
@@ -6,7 +6,8 @@
 #include <iostream>
 #include <cstring>
 
-#define MATRIX_SIZE 1024
+#define MATRIX_SIZE 4096
+#define BLOCK_SIZE 32
 
 void fill_matrix(float* matrix, uint64_t n);
 void print_matrix(float* matrix, uint64_t n);
@@ -33,6 +34,7 @@ __global__ void gpu_basic_mm(float* matrix1, float* matrix2, float* result, uint
 
 // Assumes blockDim = NxA and threadDim = B where A * B = N
 // Has 2xNxNxN total floating-point operations
+// Uses N*N total threads to calculate result
 __global__ void gpu_better_mm(float* matrix1, float* matrix2, float* result, uint64_t n)
 {
     int row = blockIdx.x;
@@ -47,38 +49,76 @@ __global__ void gpu_better_mm(float* matrix1, float* matrix2, float* result, uin
     result[column * n + row] = sum;
 }
 
-__global__ void gpu_better_transpose_mm(float* matrix1, float* matrix2, float* result, uint64_t n)
+// Uses 1 block with a 1D dimension; assumes total number of threads = N
+// Has 2xNxNxN total floating-point operations
+// A simple improvement on gpu_basic_mm() using shared memory
+__global__ void gpu_shared_mm(float* matrix1, float* matrix2, float* result, uint64_t n)
 {
-    // int r = blockIdx.x;
-    // if (threadIdx.x == 0 && blockIdx.y == 0)
-    // {
-    //     // Transpose the second matrix
-    //     int start = 1;
+    // Divide threads into indices (assuming 1D blocks)
+    //int num_threads = gridDim.x * blockDim.x;
+    int thread_index = threadIdx.x + blockIdx.x * blockDim.x;
 
-    //     for (int c = start++; c < n; ++c)
-    //     {
-    //         int temp = c * n + r;
-    //         matrix2[c * n + r] = matrix2[r * n + c];
-    //         matrix2[r * n + c] = temp;
-    //     }
-    // }
+    __shared__ float shared_row[MATRIX_SIZE];
+    __shared__ float shared_column[MATRIX_SIZE];
 
-    // int row = blockIdx.x;
-    // int column = blockIdx.y * blockDim.y + threadIdx.x;
+    // Assume num threads = n
+    for (int row = 0; row < n; ++row)
+    {
+        shared_row[thread_index] = matrix1[row * n + thread_index];
+        shared_column[thread_index] = matrix2[thread_index * n + thread_index];
+        float sum = 0;
+        for (int item = 0; item < n; ++item)
+        {
+            sum += shared_row[thread_index] * shared_column[thread_index];
+        }
+        result[thread_index * n + row] = sum;
+    }
+}
 
-    // __shared__ float shared_matrix2[MATRIX_SIZE * MATRIX_SIZE];
-    // __shared__ float shared_matrix1[MATRIX_SIZE * MATRIX_SIZE];
+// Assumes 2D blocks and 2D threads, where
+// BlockDim.x * ThreadDim.x = N
+// BlockDim.y * ThreadDim.y = N
+__global__ void gpu_optimized_mm(float* matrix1, float* matrix2, float* result, int n)
+{
+    // Assumes square tiles
+    int tile_size = blockDim.x;
 
-    // shared_matrix1[column * n + row] = matrix1[column * n + row];
-    // shared_matrix2[column * n + row] = matrix2[column * n + row];
+    // Block row and column
+    int tile_row = blockIdx.y;
+    int tile_column = blockIdx.x;
 
-    // // Assume num threads = n * n
-    // float sum = 0;
-    // for (int item = 0; item < n; ++item)
-    // {
-    //     sum += shared_matrix1[row * n + item] * shared_matrix2[item * n + column];
-    // }
-    // result[column * n + row] = sum;
+    // this block's tile
+    float* tile = &result[tile_size * tile_size * tile_row + tile_size * tile_column];
+
+    float value = 0;
+
+    // Thread row and column for this tile
+    int row = threadIdx.y;
+    int col = threadIdx.x;
+
+
+    for (int i = 0; i < (n / tile_size); ++i)
+    {
+        float *temp1 = &matrix1[tile_size * tile_size * tile_row + tile_size * i];
+        float *temp2 = &matrix2[tile_size * tile_size * i + tile_size * tile_column];
+
+        __shared__ float shared1[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ float shared2[BLOCK_SIZE][BLOCK_SIZE];
+
+        shared1[row][col] = temp1[row * tile_size + col];
+        shared2[row][col] = temp2[row * tile_size + col];
+
+        __syncthreads();
+
+        for (int j = 0; j < tile_size; ++j)
+        {
+            value += shared1[row][j] * shared2[j][col];
+        }
+
+        __syncthreads();
+    }
+
+    tile[row * tile_size + col] = value;
 }
 
 int main()
@@ -106,15 +146,8 @@ int main()
         printf("Allocation Error: %s\n", cudaGetErrorString(code));
     }
 
-    //printf("-----m1-----\n");
-    //print_matrix(m1, n);
-
-    //printf("-----m2-----\n");
-    //print_matrix(m2, n);
-
-
     // Timer start including memcpy operations
-    //auto start = std::chrono::system_clock::now();
+    //clock_t start = clock();
 
     cudaMemcpy(g_m1, m1, n * n * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(g_m2, m2, n * n * sizeof(float), cudaMemcpyHostToDevice);
@@ -128,11 +161,10 @@ int main()
     // Timer start excluding memcpy operations
     clock_t start = clock();
 
-    // has 2xnxnxn total floating-point operations
-
-    // <<<A, B>>> where A * B = N
-    gpu_basic_mm<<<32, 32>>>(g_m1, g_m2, g_result, n);
+    //gpu_basic_mm<<<32, 32>>>(g_m1, g_m2, g_result, n);
     //gpu_better_mm<<<dim3(256, 4), 1024, 2 * n * n>>>(g_m1, g_m2, g_result, n);
+    gpu_optimized_mm<<<dim3(128, 128), dim3(BLOCK_SIZE, BLOCK_SIZE)>>>(g_m1, g_m2, g_result, n);
+
     cudaThreadSynchronize();
 
     // Timer end excluding memcpy operations
@@ -147,11 +179,7 @@ int main()
     cudaMemcpy(result, g_result, n * n * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Timer end including memcpy operations
-    //float elapsed_seconds = (std::chrono::system_clock::now() - start).count();
-
-
-    //printf("-----result-----\n");
-    //print_matrix(result, n);
+    //float elapsed_seconds = (float)(clock() - start) / CLOCKS_PER_SEC;
 
     code = cudaPeekAtLastError();
     if (code != cudaSuccess)
@@ -161,11 +189,8 @@ int main()
 
     float flops = (2 * n * n * n) / elapsed_seconds;
     std::cout << "Operations: " << (2 * n * n * n) << std::endl;
-    //printf("Operations: %lld\n", (2 * n * n * n));
     printf("Seconds: %f\n", elapsed_seconds);
     std::cout << "FLOPS for gpu_basic_mm() at size " << n << " matrices = " << flops << std::endl;
-    //printf("FLOPS for gpu_basic_mm() at size %lld matrices = %f", n, flops);
-
 
     cudaFree(m1);
     cudaFree(m2);
@@ -174,9 +199,9 @@ int main()
     delete[] m1;
     delete[] m2;
     delete[] result;
-
 }
 
+// Fill matrix with random floats from 2 - 100
 void fill_matrix(float* matrix, uint64_t n)
 {
     std::uniform_real_distribution<float> distribution(2, 100);
@@ -191,6 +216,7 @@ void fill_matrix(float* matrix, uint64_t n)
     }
 }
 
+// Print matrix (for debugging purposes)
 void print_matrix(float* matrix, uint64_t n)
 {
     for (int i = 0; i < n; i++)
