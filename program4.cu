f2672f1b485e0d2ab9d47eccae10d684b0bf35b3
Ryan Berge <ryanberge13@gmail.com>
2018-06-02 17:39:29 -0700

    Set up demo for turn-in

--- program4.cu
+++ program4.cu
@@ -6,11 +6,13 @@
 #include <iostream>
 #include <cstring>
 
-#define MATRIX_SIZE 4096
 #define BLOCK_SIZE 32
 
 void fill_matrix(float* matrix, uint64_t n);
 void print_matrix(float* matrix, uint64_t n);
+void run_basic(int blocks, int threads, uint64_t n);
+void run_better(dim3 blocks, int threads, uint64_t n);
+void run_optimized(dim3 blocks, dim3 threads, uint64_t n);
 
 // Uses 1 block with a 1D dimension; assumes total number of threads = N
 // Has 2xNxNxN total floating-point operations
@@ -47,32 +49,6 @@ __global__ void gpu_better_mm(float* matrix1, float* matrix2, float* result, uin
         sum += matrix1[row * n + item] * matrix2[item * n + column];
     }
     result[column * n + row] = sum;
-}
-
-// Uses 1 block with a 1D dimension; assumes total number of threads = N
-// Has 2xNxNxN total floating-point operations
-// A simple improvement on gpu_basic_mm() using shared memory
-__global__ void gpu_shared_mm(float* matrix1, float* matrix2, float* result, uint64_t n)
-{
-    // Divide threads into indices (assuming 1D blocks)
-    //int num_threads = gridDim.x * blockDim.x;
-    int thread_index = threadIdx.x + blockIdx.x * blockDim.x;
-
-    __shared__ float shared_row[MATRIX_SIZE];
-    __shared__ float shared_column[MATRIX_SIZE];
-
-    // Assume num threads = n
-    for (int row = 0; row < n; ++row)
-    {
-        shared_row[thread_index] = matrix1[row * n + thread_index];
-        shared_column[thread_index] = matrix2[thread_index * n + thread_index];
-        float sum = 0;
-        for (int item = 0; item < n; ++item)
-        {
-            sum += shared_row[thread_index] * shared_column[thread_index];
-        }
-        result[thread_index * n + row] = sum;
-    }
 }
 
 // Assumes 2D blocks and 2D threads, where
@@ -123,9 +99,39 @@ __global__ void gpu_optimized_mm(float* matrix1, float* matrix2, float* result, 
 
 int main()
 {
-    // Size of matrices
-    uint64_t n = MATRIX_SIZE;
+    std::cout << "================== Basic ==================" << std::endl;
+    std::cout << "Block Dimension: " << 32 << ", Thread Dimension: " << 32 << ", Matrix Size: " << 1024 << std::endl;
+    run_basic(32, 32, 1024);
+    std::cout << std::endl;
 
+    std::cout << "================== Basic ==================" << std::endl;
+    std::cout << "Block Dimension: " << 64 << ", Thread Dimension: " << 64 << ", Matrix Size: " << 4096 << std::endl;
+    run_basic(32, 32, 1024);
+    std::cout << std::endl;
+
+    std::cout << "================== Better ==================" << std::endl;
+    std::cout << "Block Dimension: " << 1024 << "x" << 32 << ", Thread Dimension: " << 32 << ", Matrix Size: " << 1024 << std::endl;
+    run_better(dim3(1024, 32), 32, 1024);
+    std::cout << std::endl;
+
+    std::cout << "================== Better ==================" << std::endl;
+    std::cout << "Block Dimension: " << 4096 << "x" << 8 << ", Thread Dimension: " << 512 << ", Matrix Size: " << 4096 << std::endl;
+    run_better(dim3(4096, 8), 512, 4096);
+    std::cout << std::endl;
+
+    std::cout << "================= Optimized =================" << std::endl;
+    std::cout << "Block Dimension: " << 32 << "x" << 32 << ", Thread Dimension: " << 32 << "x" << 32 << ", Matrix Size: " << 1024 << std::endl;
+    run_optimized(dim3(32, 32), dim3(32, 32), 1024);
+    std::cout << std::endl;
+
+    std::cout << "================= Optimized =================" << std::endl;
+    std::cout << "Block Dimension: " << 128 << "x" << 128 << ", Thread Dimension: " << 32 << "x" << 32 << ", Matrix Size: " << 4096 << std::endl;
+    run_optimized(dim3(128, 128), dim3(32, 32), 4096);
+    std::cout << std::endl;
+}
+
+void run_basic(int blocks, int threads, uint64_t n)
+{
     float* m1 = new float[n * n];
     float* m2 = new float[n * n];
     float* result = new float[n * n];
@@ -161,9 +167,7 @@ int main()
     // Timer start excluding memcpy operations
     clock_t start = clock();
 
-    //gpu_basic_mm<<<32, 32>>>(g_m1, g_m2, g_result, n);
-    //gpu_better_mm<<<dim3(256, 4), 1024, 2 * n * n>>>(g_m1, g_m2, g_result, n);
-    gpu_optimized_mm<<<dim3(128, 128), dim3(BLOCK_SIZE, BLOCK_SIZE)>>>(g_m1, g_m2, g_result, n);
+    gpu_basic_mm<<<blocks, threads>>>(g_m1, g_m2, g_result, n);
 
     cudaThreadSynchronize();
 
@@ -192,8 +196,158 @@ int main()
     printf("Seconds: %f\n", elapsed_seconds);
     std::cout << "FLOPS for gpu_basic_mm() at size " << n << " matrices = " << flops << std::endl;
 
-    cudaFree(m1);
-    cudaFree(m2);
+    cudaFree(g_m1);
+    cudaFree(g_m2);
+    cudaFree(g_result);
+
+    delete[] m1;
+    delete[] m2;
+    delete[] result;
+}
+
+void run_better(dim3 blocks, int threads, uint64_t n)
+{
+    float* m1 = new float[n * n];
+    float* m2 = new float[n * n];
+    float* result = new float[n * n];
+
+    float* g_m1;
+    float* g_m2;
+    float* g_result;
+    cudaMalloc(reinterpret_cast<void**>(&g_m1), n * n * sizeof(float));
+    cudaMalloc(reinterpret_cast<void**>(&g_m2), n * n * sizeof(float));
+    cudaMalloc(reinterpret_cast<void**>(&g_result), n * n * sizeof(float));
+
+    fill_matrix(m1, n);
+    fill_matrix(m2, n);
+
+    cudaError_t code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Allocation Error: %s\n", cudaGetErrorString(code));
+    }
+
+    // Timer start including memcpy operations
+    //clock_t start = clock();
+
+    cudaMemcpy(g_m1, m1, n * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(g_m2, m2, n * n * sizeof(float), cudaMemcpyHostToDevice);
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Memcpy Error: %s\n", cudaGetErrorString(code));
+    }
+
+    // Timer start excluding memcpy operations
+    clock_t start = clock();
+
+    gpu_better_mm<<<blocks, threads>>>(g_m1, g_m2, g_result, n);
+
+    cudaThreadSynchronize();
+
+    // Timer end excluding memcpy operations
+    float elapsed_seconds = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Kernel Error: %s\n", cudaGetErrorString(code));
+    }
+
+    cudaMemcpy(result, g_result, n * n * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Timer end including memcpy operations
+    //float elapsed_seconds = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Memcpy Error 2: %s\n", cudaGetErrorString(code));
+    }
+
+    float flops = (2 * n * n * n) / elapsed_seconds;
+    std::cout << "Operations: " << (2 * n * n * n) << std::endl;
+    printf("Seconds: %f\n", elapsed_seconds);
+    std::cout << "FLOPS for gpu_better_mm() at size " << n << " matrices = " << flops << std::endl;
+
+    cudaFree(g_m1);
+    cudaFree(g_m2);
+    cudaFree(g_result);
+
+    delete[] m1;
+    delete[] m2;
+    delete[] result;
+}
+
+void run_optimized(dim3 blocks, dim3 threads, uint64_t n)
+{
+    float* m1 = new float[n * n];
+    float* m2 = new float[n * n];
+    float* result = new float[n * n];
+
+    float* g_m1;
+    float* g_m2;
+    float* g_result;
+    cudaMalloc(reinterpret_cast<void**>(&g_m1), n * n * sizeof(float));
+    cudaMalloc(reinterpret_cast<void**>(&g_m2), n * n * sizeof(float));
+    cudaMalloc(reinterpret_cast<void**>(&g_result), n * n * sizeof(float));
+
+    fill_matrix(m1, n);
+    fill_matrix(m2, n);
+
+    cudaError_t code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Allocation Error: %s\n", cudaGetErrorString(code));
+    }
+
+    // Timer start including memcpy operations
+    //clock_t start = clock();
+
+    cudaMemcpy(g_m1, m1, n * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(g_m2, m2, n * n * sizeof(float), cudaMemcpyHostToDevice);
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Memcpy Error: %s\n", cudaGetErrorString(code));
+    }
+
+    // Timer start excluding memcpy operations
+    clock_t start = clock();
+
+    gpu_optimized_mm<<<blocks, threads>>>(g_m1, g_m2, g_result, n);
+
+    cudaThreadSynchronize();
+
+    // Timer end excluding memcpy operations
+    float elapsed_seconds = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Kernel Error: %s\n", cudaGetErrorString(code));
+    }
+
+    cudaMemcpy(result, g_result, n * n * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Timer end including memcpy operations
+    //float elapsed_seconds = (float)(clock() - start) / CLOCKS_PER_SEC;
+
+    code = cudaPeekAtLastError();
+    if (code != cudaSuccess)
+    {
+        printf("Memcpy Error 2: %s\n", cudaGetErrorString(code));
+    }
+
+    float flops = (2 * n * n * n) / elapsed_seconds;
+    std::cout << "Operations: " << (2 * n * n * n) << std::endl;
+    printf("Seconds: %f\n", elapsed_seconds);
+    std::cout << "FLOPS for gpu_optimized_mm() at size " << n << " matrices = " << flops << std::endl;
+
+    cudaFree(g_m1);
+    cudaFree(g_m2);
     cudaFree(g_result);
 
     delete[] m1;
